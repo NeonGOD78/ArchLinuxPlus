238e56e1c1bfbdd670990ce126a9ec1829f8e461
NeonGOD78 <neongod@pm.me>
2025-04-03 00:11:34 +0200

    Update install.sh
    added btop

--- install.sh
+++ install.sh
@@ -423,7 +423,7 @@ microcode_detector
 
 # Pacstrap (setting up a base sytem onto the new root).
 info_print "Installing the base system (it may take a while)."
-pacstrap -K /mnt base "$kernel" "$microcode" linux-firmware "$kernel"-headers btrfs-progs grub grub-btrfs rsync efibootmgr snapper reflector snap-pac zram-generator sudo inotify-tools zsh unzip fzf zoxide colordiff curl &>/dev/null
+pacstrap -K /mnt base "$kernel" "$microcode" linux-firmware "$kernel"-headers btrfs-progs grub grub-btrfs rsync efibootmgr snapper reflector snap-pac zram-generator sudo inotify-tools zsh unzip fzf zoxide colordiff curl btop &>/dev/null
 
 #Setting Default Shell to zsh
 info_print "Setting default shell to zsh and setting up OH-MY-POSH and Zinit"
