32ed8ee4e23cb89711e5ba4f49651939873e7154
NeonGOD78 <neongod@pm.me>
2025-05-01 18:40:04 +0200

    base system

--- install.sh
+++ install.sh
@@ -1280,9 +1280,9 @@ install_base_system() {
 
   local base_packages=(
     base "$KERNEL_PACKAGE" "$MICROCODE_PACKAGE" linux-firmware "$KERNEL_PACKAGE"-headers
-    btrfs-progs grub grub-btrfs rsync efibootmgr snapper reflector snap-pac
+    "$NETWORK_PKGS" btrfs-progs grub grub-btrfs rsync efibootmgr snapper reflector snap-pac
     zram-generator sudo bash-completion inotify-tools zsh unzip unrar fzf zoxide colordiff curl
-    btop mc git systemd ukify openssl sbsigntools sbctl base-devel go "$NETWORK_PKGS"
+    btop mc git systemd ukify openssl sbsigntools sbctl base-devel go dracut --ignore mkinitcpio --ignore mkinitcpio-busybox 
   )
 
   enable_debug
