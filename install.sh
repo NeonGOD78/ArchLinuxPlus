3eb38912ceb709bdc39437f66a755e82741cf043
NeonGOD78 <neongod@pm.me>
2025-05-01 10:04:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1281,8 +1281,7 @@ install_base_system() {
     base "$KERNEL_PACKAGE" "$MICROCODE_PACKAGE" linux-firmware "$KERNEL_PACKAGE"-headers
     btrfs-progs grub grub-btrfs rsync efibootmgr snapper reflector snap-pac
     zram-generator sudo bash-completion inotify-tools zsh unzip unrar fzf zoxide colordiff curl
-    btop mc git systemd ukify openssl sbsigntools sbctl base-devel
-    "$NETWORK_PKGS"
+    btop mc git systemd ukify openssl sbsigntools sbctl base-devel go "$NETWORK_PKGS"
   )
 
   enable_debug
